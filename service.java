42c835c61b2ef38ec9db26d71714fbaac0391080
Nabeel4455 <103034544+Nabeel4455@users.noreply.github.com>
2022-05-04 16:32:18 +0500

    Updated

--- service.java
+++ service.java
@@ -1,8 +1,36 @@
-// Your First Program
+class Main {
 
-class HelloWorld {
+  // first function
+  public int addNumbers(int a, int b) {
+    int sum = a + b;
+   
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    
+    int num1 = 25;
+    int num2 = 15;
+
+    // create an object of Main
+    Main obj = new Main();
+    // calling method
+    int result = obj.addNumbers(num1, num2);
+    System.out.println("Sum is: " + result);
+  }
+
+// second function
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
+    
+ // third function (failed test)
+ public static void main (String[] args){
+    System.out.println("Hello , this is sample program");   
+    double d = Math.random();
+    System.out.println("Random Number:"+d)
+ }
+ 
+
     
 }
