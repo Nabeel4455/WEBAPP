95b53e5a2db4dd0c68ea31ca91d387655e688821
Nabeel4455 <103034544+Nabeel4455@users.noreply.github.com>
2022-05-04 17:24:21 +0500

    Update service.java

--- service.java
+++ service.java
@@ -6,7 +6,21 @@ class Main {
    
     return sum;
   }
-
+  public int prodNumbers(int a, int b) {
+    int prod = a * b;
+    // return value
+    return prod;
+  }
+  
+ public static void findEvenOdd(int num)  
+{  
+//method body  
+if(num%2==0)   
+System.out.println(num+" is even");   
+else   
+System.out.println(num+" is odd");  
+}  
+ 
   public static void main(String[] args) {
     
     int num1 = 25;
@@ -16,21 +30,11 @@ class Main {
     Main obj = new Main();
     // calling method
     int result = obj.addNumbers(num1, num2);
+    int result2 = obj.prodNumbers(num1, num2);
+    obj.findEvenOdd(num1);
     System.out.println("Sum is: " + result);
-  }
-
-// second function
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+    System.out.println("Product is: " + result2);
     
- // third function (failed test)
- public static void main (String[] args){
-    System.out.println("Hello , this is sample program");   
-    double d = Math.random();
-    System.out.println("Random Number:"+d)
- }
- 
-
+  }
     
 }
